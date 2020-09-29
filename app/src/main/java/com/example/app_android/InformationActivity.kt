162b19574ec5d9a6c99cefec47e948cc018700f5
Huy/taurusilver <taurusilver310@gmail.com>
2020-09-29 16:10:32 +0700

    navigation_bottom all layout

--- app/src/main/java/com/example/app_android/InformationActivity.kt
+++ app/src/main/java/com/example/app_android/InformationActivity.kt
@@ -1,11 +1,35 @@
 package com.example.app_android
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class InformationActivity:AppCompatActivity() {
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import me.ibrahimsn.lib.SmoothBottomBar
+
+
+class InformationActivity:AppCompatActivity(R.layout.activity_barbottom) {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        navController = findNavController(R.id.main_fragment)
+        setupActionBarWithNavController(navController)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.icon_resource,menu)
+        findViewById<SmoothBottomBar>(R.id.bar_bottomBar).setupWithNavController(menu!!,navController)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
+    }
+
+
 }
