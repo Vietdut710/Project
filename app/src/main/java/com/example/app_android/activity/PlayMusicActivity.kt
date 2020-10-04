7d7934a8a8f5ae4a367aa2957248a01717bf6875
LAPTOP-EUH1F0KC\huyho <taurusilver310@gmail.com>
2020-10-04 10:55:06 +0700

    update

--- app/src/main/java/com/example/app_android/activity/PlayMusicActivity.kt
+++ app/src/main/java/com/example/app_android/activity/PlayMusicActivity.kt
@@ -1,19 +1,21 @@
 package com.example.app_android.activity
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD:app/src/main/java/com/example/app_android/PlayMusicActivity.kt
+import com.example.app_android.R
 import kotlinx.android.synthetic.main.activity_play_music.*
 
 class PlayMusicActivity:AppCompatActivity()  {
 
     private lateinit var media: MediaPlayer
     private var totalTime: Int = 0
-=======
-import com.example.app_android.R
->>>>>>> Thanh_branch:app/src/main/java/com/example/app_android/activity/PlayMusicActivity.kt
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +25,80 @@ import com.example.app_android.R
         media.isLooping = true
         media.setVolume(0.5f, 0.5f)
         totalTime = media.duration
+
+        play_music_bar_volume.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekbar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    if (fromUser) {
+                        var volumeNum = progress / 100.0f
+                        media.setVolume(volumeNum, volumeNum)
+                    }
+                }
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                }
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                }
+            }
+        )
+        play_music_time_music.max = totalTime
+        play_music_time_music.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    if (fromUser) {
+                        media.seekTo(progress)
+                    }
+                }
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                }
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                }
+            }
+        )
+
+        // Thread
+        Thread(Runnable {
+            while (media != null) {
+                try {
+                    var msg = Message()
+                    msg.what = media.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                }
+            }
+        }).start()
     }
+
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            var currentPosition = msg.what
+
+            // Update positionBar
+            play_music_time_music.progress = currentPosition
+
+            // Update Labels
+            var elapsedTime = createTimeLabel(currentPosition)
+            play_list_start_time.text = elapsedTime
+
+            var remainingTime = createTimeLabel(totalTime - currentPosition)
+            play_list_end_time.text = "-$remainingTime"
+        }
+    }
+
+    fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        var min = time / 1000 / 60
+        var sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
+    }
+
+
 
     fun BtnPlayOnClick(view: View){
         if(media.isPlaying){
@@ -34,7 +109,6 @@ import com.example.app_android.R
             media.start()
             play_music_play.setBackgroundResource(R.drawable.ic_stop)
         }
-    }
+    }}
 
 
-}
--- app/src/main/java/com/example/app_android/activity/SongActivity.kt
+++ app/src/main/java/com/example/app_android/activity/SongActivity.kt
@@ -1,11 +1,15 @@
 package com.example.app_android.activity
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.app_android.R
 import com.example.app_android.`object`.Song
 import com.example.app_android.adapter.SongAdapter
+import kotlinx.android.synthetic.main.activity_song.*
+import kotlinx.android.synthetic.main.view_song.*
 
 class SongActivity : AppCompatActivity() {
     var songViewPlaylist : ListView ?= null
@@ -14,7 +18,6 @@ class SongActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song)
-
 
         songList = ArrayList()
         songList?.add(Song(1,"Đảng đã cho ta mùa xuân"))
@@ -31,4 +34,8 @@ class SongActivity : AppCompatActivity() {
         songViewPlaylist?.adapter = songAdapter
 
     }
+
+
+
 }
+
